89d248d235996f7637114044d4a0b31302cb2f3f
BlackPlayer10 <luigi.cabrera.h@hotmail.com>
2024-02-29 15:22:22 -0500

    On branch Preshi Changes to be committed: 	modified:   Scenes/boss.tscn 	new file:   Scripts/Boss1_attack1.gd 	new file:   Scripts/Boss1_idle.gd 	modified:   Scripts/Turing M.gd 	new file:   Scripts/TuringMachineAI.gd 	modified:   Scripts/boss.gd
    Marcelo's Turing Machine code has been deactivated
    
    Boss script has been refactor
    _on_animation_player_animation_finished method was added to boss script and its connected to animation finished signal
    We use this in order to know when an attack or another kind of animation is finished.
    When that happens, Turing Machine state may change
    
    Turing machine AI code can be used for enemies too
    At _ready method it loops all children looking for the states of the boss (Uses a dictionary to save them)
    It uses the updateState method to change to another state (saves and exit previous state and inits the new state)
    
    Boss1_idle and boss1_attack1 are states of TM animation
    The enter function sets global animation vars to true (animation starts playing)
    When the animation has finished this globals will be set to false in _on_animation_player_animation_finished method (boss script)
    The possibilities will chose the next state as long as animaition global is set to false
    Possibilities for not changing the state can exist, in that case we will need to reset the animation and set the animation global var to true again (Enter method)

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -1,5 +1,40 @@
 extends CharacterBody2D
 
+@onready var player = $"."
+@onready var sprite = $Sprite2D
+
+var direction : Vector2
+var dmg = 50
+var HP = 50*16
+
+'''Animation global vars to know if an animation has finished'''
+var animations = {'Idle': false, 'Attack Right': false, 'Beam': false}
+
+
+func _ready():
+	set_physics_process(false) # Physisc are processed on TM AI states
+	
+
+func _process(_delta):
+	direction = player.position - position
+	sprite.flip_h = direction.x<0 # Flip sprites towards player
+	
+
+'''Function called when an animation finishes (Set var to false and turing machine may change state)'''
+func _on_animation_player_animation_finished(anim_name):
+	print("\nAnimation: ", anim_name, " has finished\n")
+	if anim_name not in animations.keys():
+		print("Not in keys")
+		return
+	animations[anim_name] = false
+
+
+
+
+'''Marcelo's Code '''
+'''
+extends CharacterBody2D
+
 @onready var pj = get_parent().find_child("Player")
 @onready var sprite = $Sprite2D
 
@@ -19,7 +54,12 @@ func  _process(_delta):
 		sprite.flip_h =false
 		
 func _physics_process(delta):
-	velocity = direction.normalized()*50 # 200
+	velocity = direction.normalized()*50 # 200 (velo)
 	move_and_collide(velocity * delta)
+	
 func dmgPlayer():
 	pj.take_damage(dmg)
+'''
+
+
+
